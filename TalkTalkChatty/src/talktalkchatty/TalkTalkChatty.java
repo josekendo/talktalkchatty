e6c521cddcb6126e89bc8ab42b825682bbe01b90
josekendo <josevicenteantoncoy@hotmail.com>
2017-09-26 14:03:29 +0200

    unos archivitos mas

--- TalkTalkChatty/src/talktalkchatty/TalkTalkChatty.java
+++ TalkTalkChatty/src/talktalkchatty/TalkTalkChatty.java
@@ -14,8 +14,10 @@ public class TalkTalkChatty {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         // TODO code application logic here
+        
     }
     
 }
