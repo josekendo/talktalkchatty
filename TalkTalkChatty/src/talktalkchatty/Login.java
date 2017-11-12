b5bfd5d2c635605640d012ff74a0473864f2e238
Marinés <mia.castello.k94@gmail.com>
2017-11-12 01:33:03 +0100

    Revert "Clase Imagen sacada de Login"
    This reverts commit a965a74cda53de3b7082cb22e6539dcc048fc4b8.

--- TalkTalkChatty/src/talktalkchatty/Login.java
+++ TalkTalkChatty/src/talktalkchatty/Login.java
@@ -25,6 +25,25 @@ public class Login extends javax.swing.JFrame {
         jPanel1.repaint();
         wrong.setVisible(false);
     }
+     public class Imagen extends javax.swing.JPanel { 
+        public Imagen() { 
+            this.setSize(100, 100); //se selecciona el tamaño del panel 
+        } 
+ 
+        //Se crea un método cuyo parámetro debe ser un objeto Graphics 
+        public void paint(Graphics grafico) { 
+            Dimension height = getSize(); 
+ 
+            //Se selecciona la imagen que tenemos en el paquete de la //ruta del programa 
+            ImageIcon Img = new ImageIcon(getClass().getResource("logoTTC.png"));  
+ 
+            //se dibuja la imagen que tenemos en el paquete Images //dentro de un panel 
+            grafico.drawImage(Img.getImage(), 0, 0, height.width, height.height, null); 
+ 
+            setOpaque(false); 
+            super.paintComponent(grafico); 
+        } 
+    } 
      
      
      
