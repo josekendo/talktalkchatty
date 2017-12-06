fad21775bd2b429b2d1a03164dc5d5f1eca222eb
JoseVIcente <josevicenteantoncoy@hotmail.com>
2017-12-06 16:12:57 +0100

    primera version seguridad y almacenamiento
    en almacenamiento falta las gestiones de conversaciones y sus recuperaciones

--- TalkTalkChatty/src/talktalkchatty/almacenamiento.java
+++ TalkTalkChatty/src/talktalkchatty/almacenamiento.java
@@ -5,10 +5,171 @@
  */
 package talktalkchatty;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /**
  *
  * @author JVAC
  */
 public class almacenamiento {
+        //todo en esta clase se guarda en formato texto la encriptacion se hace con la clave aes secreta personal e intransferible 
+    private int contadorIDS;
+    //recupera nuestras claves si existen
+    public void recuperarclaves(seguridad se,String clave)
+    {
+      File archivo=new File("keys.dat");
+      if(archivo.exists())
+      {
+          String lineas[] = leer(archivo);
+          if(lineas.length >= 3)
+          {
+              se.loadcp(lineas[0]);
+              se.loadcpr(lineas[1]);
+              se.crearSecreta(clave);
+              se.crearSessionAes();
+              //aqui tenemos todas las claves nuestras
+          }
+      }  
+    }
     
+    //guarda nuestras claves
+    public void guardarClaves(String clavepublica,String claveprivada,String clavesecreta)
+    {
+        File directorio=new File("keys.dat");
+        if(directorio.exists())
+        {
+            //sobreescribimos el archivo
+            //creamos el archivo
+            try{
+                directorio.createNewFile();
+                try (BufferedWriter bw = new BufferedWriter(new FileWriter(directorio))) {
+                    bw.write(clavepublica);//primera linea clave publica
+                    bw.newLine();
+                    bw.write(claveprivada);//segunda linea clave privada
+                    bw.newLine();
+                    bw.write(clavesecreta);//tercera linea clave secreta
+                    bw.newLine();
+                } //primera linea clave publica
+            }catch(IOException ex)
+            {
+                System.out.println(ex);
+            }            
+        }
+        else
+        {
+            //creamos el archivo
+            try{
+            directorio.createNewFile();
+                try (BufferedWriter bw = new BufferedWriter(new FileWriter(directorio))) {
+                    bw.write(clavepublica);//primera linea clave publica
+                    bw.newLine();
+                    bw.write(claveprivada);//segunda linea clave privada
+                    bw.newLine();
+                    bw.write(clavesecreta);//tercera linea clave secreta
+                    bw.newLine();
+                } //primera linea clave publica
+            }catch(IOException ex)
+            {
+                System.out.println(ex);
+            }
+        }
+    }
+    
+    
+    //nos sirve para crear un nuevo usuario en el sistema local 
+    public void crearNuevoUsuarioLocal(String email, String VerificacionPassword,String nombre, String foto,String id)
+    {
+        //creamos una carpeta con el email 
+        //File directorio=new File(email.replace("@", "arroba"));
+        File directorio=new File(email);
+        directorio.mkdir();   
+        //creamos un archivo vef que contenga la varificacion del password
+        String ruta = email+"/vef";//datos del usuario
+        String ruta2 = email+"/chat";//este archivo actua como pila si le envian un mensaje se almacena y luego se procesa cuando se confirma se borra esa linea
+        File archivo2 = new File(ruta2);
+        File archivo = new File(ruta);
+        BufferedWriter bw;
+        if(!archivo.exists()) 
+        {
+            try
+            {
+                archivo.createNewFile();
+                archivo2.createNewFile();
+                bw = new BufferedWriter(new FileWriter(archivo));
+                bw.write(VerificacionPassword);//password
+                bw.newLine();
+                bw.write(nombre);//nombre
+                bw.newLine();
+                bw.write(foto);//foto
+                bw.newLine();
+                bw.write(id);//id
+                bw.newLine();
+                bw.close();
+            }catch(IOException ex)
+            {
+                System.out.println("fallo al crear los archivos "+ex.getLocalizedMessage());
+            }
+        }
+    }
+    
+   
+    //esto nos sirve para saber si existe el usuario(en cliente)
+    public boolean existeUsuarioLocal(String email)
+    {
+        File directorio=new File(email);
+        return directorio.exists();
+    }
+    
+    
+    //lee un fichero y devuelve si un string
+    public String[] leer(File archivo)
+    {
+        String cadena,cadena3;
+        cadena3="";
+        try{
+        String cadena2 [];
+        int contador = 0;
+        FileReader f = new FileReader(archivo);
+        try (BufferedReader b = new BufferedReader(f)) 
+        {
+            while((cadena = b.readLine())!=null)
+            {
+                if(contador == 0)
+                {
+                    cadena3=cadena3+cadena;
+                }
+                else
+                {
+                    cadena3=cadena3+"marijose"+cadena;
+                }
+                contador++;
+            }   
+        }
+        if(contador == 1)
+        {
+            cadena2 = new String[1];
+            cadena2[0] = cadena3;
+        }
+        else
+        {
+            cadena2 = cadena3.split("marijose");
+        }
+        return cadena2;
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(almacenamiento.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(almacenamiento.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
 }
--- TalkTalkChatty/src/talktalkchatty/seguridad.java
+++ TalkTalkChatty/src/talktalkchatty/seguridad.java
@@ -5,10 +5,417 @@
  */
 package talktalkchatty;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
+
 /**
  *
  * @author JVAC
  */
 public class seguridad {
     //se implementara apartir de la del servidor
+        
+    private PublicKey clavePublica;//clave publica (para compartir servidor y demas usuarios)
+    private PrivateKey clavePrivada;//clave privada(solo nosotros)
+    private Key claveSecreta;//clave secreta es utilizada para los archivos internos (solo nosotros)
+    private Key claveSession;//clave de session se crea cada vez que se abre el programa (para compartir solo con usuarios)
+    private ArrayList<Key> clave_session;
+    private ArrayList<String> nombre;
+    private ArrayList<PublicKey> claves_publicas;
+    
+    //inicializamos a null;
+    public void seguridad()
+    {
+        this.clavePrivada = null;
+        this.clavePublica = null;
+        this.claveSecreta = null;
+        this.claveSession = null;
+        this.clave_session = new ArrayList();
+        this.nombre = new ArrayList();
+        this.claves_publicas = new ArrayList();
+    }
+    
+    //genera un par de claves nuevas(solo ejecutar si no se encuentra archivo de inicio)
+    public boolean crearrsa()
+    {
+        KeyPairGenerator keyPairGenerator;
+        try {
+            keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            clavePublica = keyPair.getPublic();//se genera la clave publica
+            clavePrivada = keyPair.getPrivate();//se genera la clave privada
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(seguridad.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+    
+    //genera una clave de session nueva
+    public void crearSessionAes()
+    {
+        KeyGenerator keyGenerator;
+        try {
+            keyGenerator = KeyGenerator.getInstance("AES");
+            keyGenerator.init(128);
+            this.claveSession = keyGenerator.generateKey();//creada
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(seguridad.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    //genera una clave de aes secreta nueva (solo se crea al entrar por primera vez el servidor o cliente)
+    public void crearSecreta(String clave)
+    {
+      try{
+      KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+      keyGenerator.init(128);
+      this.claveSecreta = new SecretKeySpec(clave.getBytes(),0, 16, "AES");
+      }catch(NoSuchAlgorithmException ex)
+      {
+          System.out.println(ex);
+      }
+    }
+    
+    //encripta con la clave publica que le pasemos
+    public String encriptarPublica(PublicKey clave,String mensaje)
+    {
+        try {
+            Cipher rsa;
+            rsa = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            rsa.init(Cipher.ENCRYPT_MODE, clave);
+            byte[] encriptado = rsa.doFinal(mensaje.getBytes());
+            String men = "";
+            int con = 0;
+            for (byte b : encriptado) {
+                if(con == 0)
+                {
+                   men = men+(Integer.toHexString(0xFF & b)); 
+                }
+                else
+                {
+                   men = men+"@loki#"+(Integer.toHexString(0xFF & b));
+                }
+                con++;
+            }
+            return men;
+        } catch (InvalidKeyException | NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException ex) {
+            Logger.getLogger(seguridad.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    //desencripta con nuestra clave privada
+    public String desencriptarPrivada(String mensaje)
+    {
+        try {
+        Cipher rsa;
+        rsa = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        rsa.init(Cipher.DECRYPT_MODE, this.clavePrivada);//se desencripta siempre con nuestra clave privada
+        String [] men = mensaje.split("@loki#");
+        byte[] mensaje_en_bytes = new byte[men.length];
+        int con = 0;
+        for(String s : men)
+        {
+            mensaje_en_bytes[con] = (byte)Integer.parseInt(s,16);//Integer.parseInt(s,16)
+            con++;
+        }
+        byte[] desencriptado = rsa.doFinal(mensaje_en_bytes);
+        String des = new String(desencriptado);
+        return des;
+        }catch(InvalidKeyException | NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException ex)
+        {
+           System.out.println(ex);
+        }
+        return null;
+    }
+    
+    //encriptamos con la clave de session 
+    public String encriptarSession(Key session,String m)
+    {
+          // Se obtiene un cifrador AES
+          try
+          {
+            Cipher aes = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            aes.init(Cipher.ENCRYPT_MODE, session);
+            byte[] encriptado = aes.doFinal(m.getBytes());
+            String men = "";
+            int con = 0;
+            for (byte b : encriptado) {
+                if(con == 0)
+                {
+                   men = men+(Integer.toHexString(0xFF & b)); 
+                }
+                else
+                {
+                   men = men+"@thor#"+(Integer.toHexString(0xFF & b));
+                }
+                con++;
+            }
+            return men;
+          }
+          catch(InvalidKeyException | NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException e)
+          {
+              System.out.println(e);
+          }
+          return null;
+    }
+    //desencriptamos con la clave aes que nos pasen -- sera utilizada para desencriptar los archivos al iniciar el programa
+    public String desencriptarAes(Key clave, String m)
+    {
+        try
+        {
+            Cipher aes = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            aes.init(Cipher.DECRYPT_MODE, clave);
+            String [] men = m.split("@thor#");
+            byte[] mensaje_en_bytes = new byte[men.length];
+            int con = 0;
+            for(String s : men)
+            {
+                mensaje_en_bytes[con] = (byte)Integer.parseInt(s,16);//Integer.parseInt(s,16)
+                con++;
+            }
+            byte[] desencriptado = aes.doFinal(mensaje_en_bytes);
+            String des = new String(desencriptado);
+            return des;
+        }catch(InvalidKeyException | NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException e)
+        {
+            System.out.println(e);
+        }
+        return null;
+    }
+    //crea la hash del password 
+    public String sha512(String password)
+    {
+        try{
+            MessageDigest md= MessageDigest.getInstance("SHA-512");
+            md.update(password.getBytes());
+            byte[] mb = md.digest();
+            //Arrays.toString(mb) comprobante
+            byte[] encoded = Base64.encodeBase64(mb);
+            System.out.println(new String(encoded));
+            return new String(encoded,"UTF-8");
+        }catch(UnsupportedEncodingException | NoSuchAlgorithmException ex)
+        {
+            System.out.println(ex);
+        }
+        return null;
+    }
+    
+    //guarda clave aes secreta, clave aes publica y clave aes privada
+    //las claves de session de cada usuario se guardan en un archivo individual desde almacenamiento
+    public void guardarMisClaves()
+    {
+        if(this.clavePrivada != null && this.clavePublica != null && this.claveSecreta != null)
+        {
+            byte[] ncp = this.clavePublica.getEncoded();
+            byte[] ncpr = this.clavePrivada.getEncoded();
+            byte[] ncs = this.claveSecreta.getEncoded();
+            almacenamiento al = new almacenamiento();
+            al.guardarClaves(new String(Base64.encodeBase64(ncp)),new String(Base64.encodeBase64(ncpr)),new String(Base64.encodeBase64(ncs)));
+        }
+    }
+    
+    //devolvemos nuestra clave publica generada la primera ejecucion
+    public PublicKey getClavePublica()
+    {
+        return this.clavePublica;//devolvemos nuestra clave publica
+    }
+
+    //devolvemos nuestra clave publica generada la primera ejecucion para enviarla a un cliente o servidor
+    public String getClavePublica_envio()
+    {
+        byte[] ncp = this.clavePublica.getEncoded();
+        String clave = new String(Base64.encodeBase64(ncp));
+        return clave;//devolvemos nuestra clave publica
+    }
+    
+    //recuperamos nuestra clave de session generada al arrancar el cliente o el servidor
+    public Key getClaveSession()
+    {
+        return this.claveSession;//devolvemos nuestra clave publica
+    }
+    
+    //devolvemos nuestra clave publica generada la primera ejecucion para enviarla a un cliente o servidor
+    public String getClaveSession_envio()
+    {
+        byte[] ncp = this.claveSession.getEncoded();
+        String clave = new String(Base64.encodeBase64(ncp));
+        return clave;//devolvemos nuestra clave publica
+    }
+    //recuperamos la clave privada
+    public boolean loadcpr(String clave)
+    {
+        byte[] clavepreB = Base64.decodeBase64(clave);
+        byte[] claveB = clavepreB;
+        try
+        {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            KeySpec keySpec = new PKCS8EncodedKeySpec(claveB);
+            PrivateKey keyFromBytes = keyFactory.generatePrivate(keySpec);
+            this.clavePrivada = keyFromBytes;
+            return true;
+        }catch(NoSuchAlgorithmException | InvalidKeySpecException ex)
+        {
+            System.out.println(ex);
+        }
+        return false;
+    }
+    //recuperamos la clave secreta o session
+    public boolean loadcs(String clave)
+    {
+        byte[] clavepreB = Base64.decodeBase64(clave);
+        byte[] claveB = clavepreB;
+        try
+        {
+            KeyFactory keyFactory = KeyFactory.getInstance("AES");
+            Key key = new SecretKeySpec(claveB, "AES");
+            this.claveSecreta = key;
+            return true;
+        }catch(NoSuchAlgorithmException ex)
+        {
+            System.out.println(ex);
+        }
+        return false;
+    }
+    //recuperamos la clave secreta o session
+    public Key loadcs_externa(String clave)
+    {
+        byte[] clavepreB = Base64.decodeBase64(clave);
+        byte[] claveB = clavepreB;
+        try
+        {
+            KeyFactory keyFactory = KeyFactory.getInstance("AES");
+            Key key = new SecretKeySpec(claveB, "AES");
+            return key;
+        }catch(NoSuchAlgorithmException ex)
+        {
+            System.out.println(ex);
+        }
+        return null;
+    }
+    //recuperamos la clave publica
+    public boolean loadcp(String clave)
+    {
+        byte[] clavepreB = Base64.decodeBase64(clave);
+        byte[] claveB = clavepreB;
+        try
+        {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            KeySpec keySpec = new X509EncodedKeySpec(claveB);
+            PublicKey keyFromBytes = keyFactory.generatePublic(keySpec);
+            this.clavePublica = keyFromBytes;
+            return true;
+        }catch(NoSuchAlgorithmException | InvalidKeySpecException ex)
+        {
+            System.out.println(ex);
+        }
+        return false;
+    }
+    //recuperamos la clave publica
+    public PublicKey loadcp_externa(String clave)
+    {
+        byte[] clavepreB = Base64.decodeBase64(clave);
+        byte[] claveB = clavepreB;
+        try
+        {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            KeySpec keySpec = new X509EncodedKeySpec(claveB);
+            PublicKey keyFromBytes = keyFactory.generatePublic(keySpec);
+            return keyFromBytes;
+        }catch(NoSuchAlgorithmException | InvalidKeySpecException ex)
+        {
+            System.out.println(ex);
+        }
+        return null;
+    }
+    //le pasas(nombre,email o id) segun como se inicialice, te devuelve la clave publica de ese usuario para encriptar los mensajes que le vas a enviar
+    public PublicKey devolver_publica(String n)
+    {
+        if(!this.nombre.isEmpty())
+        {
+            if(!this.claves_publicas.isEmpty())
+            {
+                for(int a=0;a < nombre.size();a++)
+                {
+                    if(nombre.get(a) != null && n.compareToIgnoreCase(nombre.get(a)) == 0)
+                    {
+                        if(a < claves_publicas.size())
+                        {
+                            return claves_publicas.get(a);
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }   
+        }
+        return null;
+    }
+    //le pasas el nombre o id o email segun como se inicialice y te devuelve la clave de session para desencriptar o encriptar los mensajes y archivos que le vas a enviar  
+    public Key devolver_session(String n)
+    {
+        if(!this.nombre.isEmpty())
+        {
+            if(!this.clave_session.isEmpty())
+            {
+                for(int a=0;a < nombre.size();a++)
+                {
+                    if(nombre.get(a) != null && n.compareToIgnoreCase(nombre.get(a)) == 0)
+                    {
+                        if(a < this.clave_session.size())
+                        {
+                            return clave_session.get(a);
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }                
+            }               
+        }
+        return null;
+    }
+    //creamos un usuario(id) y su clave publica y privada
+    public void crearUser(String n,Key session, PublicKey publica)
+    {
+        this.nombre.add(n);
+        this.clave_session.add(session);//esto se agrega cuando tenemos su id del server
+        this.claves_publicas.add(publica);//esto se agrega cuando tenemos su id del server
+    }
+    //funcion para encriptar con nuestra clave aes y su publica 
+    public String encriptarMiSessionSuPublica(String id,String mensaje)//id del usuario al que se va a enviar
+    {
+        return this.encriptarPublica(this.devolver_publica(id), this.encriptarSession(this.claveSession,mensaje));
+    }
+    //version mas robusta se encripta primero con mi session luego con su session y por ultimo con su publica(no utilizada por comodidad)
+    public String encriptarMiSessionSuSessionSuPublica(String id,String mensaje)//id del usuario al que se va enviar
+    {
+        return this.encriptarPublica(this.devolver_publica(id),this.encriptarSession(this.devolver_session(id),this.encriptarSession(this.claveSession,mensaje)));    
+    }
+
 }
